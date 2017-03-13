f9d7ab4d521e6ce3d918b2d7b6557dcb7c83850f
chengang <372763861@qq.com>
2017-03-13 14:48:33 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,3 +2,36 @@
 
 nohup /bin/supernode -l 10082 &
 nohup /bin/supernode2 -l 10086 -f -v &
+
+suiji=$RANDOM
+if 	[ "$suiji" = "" ];then 
+suiji=1$(date "+%M%S")
+fi
+cat > "/frpc.ini" <<EOF
+[common]
+server_addr = frp.lu8.win
+server_port = 7000
+privilege_token = frp888
+log_file = frpc.log
+log_level = info
+log_max_days = 3
+
+[udp_$suiji]
+privilege_mode = true
+type = udp
+local_ip = 127.0.0.1
+local_port = 10082
+use_gzip = true
+use_encryption = true
+remote_port = 10082
+
+[udp_$suiji]
+privilege_mode = true
+type = udp
+local_ip = 127.0.0.1
+local_port = 10086
+use_gzip = true
+use_encryption = true
+remote_port = 10086
+
+EOF
